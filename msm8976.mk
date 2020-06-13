328fa8d7a7f99a1c277677dd9e0d76e4983fa7ea
HiterretiH <bronik88888888@gmail.com>
2020-06-13 18:12:17 +0300

    Lineage-ify

--- msm8976.mk
+++ msm8976.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019 The MoKee Project
+# Copyright (C) 2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ PRODUCT_AAPT_CONFIG := normal
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-mokee
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 # ANT+
 PRODUCT_PACKAGES += \
